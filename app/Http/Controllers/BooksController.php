9a742f77dab215a012900c8195193d917663beab
jame <jamehophop@gmail.com>
2020-03-09 20:32:38 +0700

    edit and Controller

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,9 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Books;
+use App\Books;   // เรียกใช้ โมเดล Booksที่สร้างขึ้นมา
 use App\Http\Requests\StoreBooksRequest;
-use image;
+use Image;
 use File;
 
 class BooksController extends Controller
@@ -17,8 +17,9 @@ class BooksController extends Controller
      */
     public function index()
     {
-        $books = Books::with('typebooks')->orderBy('id','desc')->paginate(5);
+        $books=Books::with('typebooks')->orderBy('id','desc')->paginate(3);
         return view('books/index',['books'=>$books]);
+
     }
 
     /**
@@ -28,7 +29,7 @@ class BooksController extends Controller
      */
     public function create()
     {
-        return view('books.create');
+     return view('books.create');
     }
 
     /**
@@ -40,22 +41,23 @@ class BooksController extends Controller
     public function store(StoreBooksRequest $request)
     {
         $book = new Books();
-        $book->title = $request->title;
+        $book->title =  $request->title;
         $book->price = $request->price;
         $book->typebooks_id = $request->typebooks_id;
         if($request->hasFile('image')){
             $filename = str_random(10).'.'.$request->file('image')->getClientOriginalExtension();
-            $request->file('image')->move(public_path().'/images',$filename);
-            Image::make(public_path().'/images'.$filename)->resize(50,50)->save(public_path().'/images/resize'.$filename);
+            $request->file('image')->move(\public_path(). '/image/',$filename);
+            Image::make(\public_path(). '/image/'.$filename)->resize(50,50)->save(\public_path().'/image/resize/'.
+            $filename);
             $book->image = $filename;
         } else {
-            $book ->image ='nopic.jpg';
+            $book->image = 'nopic.jpg';
+        }
+        $book->save();
+        return redirect()->action('BooksController@index');
 
-        }
-            $book->save();
-            return redirect()->action('BooksController@index');
-        }
-    
+
+    }
 
     /**
      * Display the specified resource.
@@ -87,25 +89,29 @@ class BooksController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(StoreBooksRequest $request, $id)
+    public function update(Request $request, $id)
     {
-        $book = new Books();
+        $book = Books::find($id);
         $book->title = $request->title;
         $book->price = $request->price;
         $book->typebooks_id = $request->typebooks_id;
-        if($request->hasFile('image')){
-            if($book->image !='nopic.jpg'){
-                File::delete(public_path(). '\\images\\'.$book->image);
-                File::delete(public_path(). '\\images\\resize\\'.$book->image);
+
+        if ($request->hasFile('image')){
+            // delete old file before update
+            if($book->imahe !='nopic.jpg'){
+                File::delete(public_path().'\\images\\'.$book->image);
+                File::delete(public_path().'\\images\\resize\\'.$book->image);
             }
             $filename = str_random(10).'.'.$request->file('image')->getClientOriginalExtension();
-            $request->file('image')->move(public_path(). '/images/',$filename);
-            Image::make(public_path().'/images/'.$filename)->resize(50,50)->save(public_path().'/images/resize/'.$filename);
+            $request->file('image')->move(public_path().'/images/',$filename);
+            image::make(public_path().'/images/'.$filename)->resize(50,50)->save(public_path().'/images/resize'.
+            $filename);
             $book->image = $filename;
+        }
+        $book->save();
+        return redirect()->action('BooksController@index');
     }
-    $book->save();
-    return redirect()->action('BooksController@index');
-    }
+
     /**
      * Remove the specified resource from storage.
      *
@@ -115,9 +121,10 @@ class BooksController extends Controller
     public function destroy($id)
     {
         $book = Books::find($id);
-        if($book->image !='nopic.jpg'){
-            File::delete(public_path(). '\\images\\'.$book->image);
-            File::delete(public_path(). '\\images\\resize\\'.$book->image);
+        if($book->image != 'nopic.jpg'){
+            File::delete(public_path().'\\images\\'.$book->image);
+            File::delete(public_path().'\\images\\resize\\'.$book->image);
+
         }
         $book->delete();
         return redirect()->action('BooksController@index');
